7b55b698399843ba4053430ad37aa921a0288289
r1ader <r1ader@qq.com>
2022-05-11 23:42:47 +0800

    exper(hooks): implement useEvent

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,38 @@
 import Head from "next/head";
-import { useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import axios from "axios";
+import { useEvent } from "../util/hooks";
 
+let origin = null;
 export default function Home() {
   const [num, setNum] = useState(0);
-  useEffect(async () => {
-    setNum((await axios.get("/api/person")).data.text);
+  const add = useCallback(() => {
+    setNum(num + 1);
+  }, [num]);
+  const con = useEvent(() => {
+    console.log(num);
+  }, [num]);
+  useEffect(() => {
+    origin = con;
+    console.log(origin === con);
   }, []);
+  console.log(origin === con);
   return (
     <div className="container">
       <Head>
         <title>r1ader</title>
         <link rel="icon" href="/favicon.ico"/>
       </Head>
-      <main>
-        <div>{num}</div>
+      <main style={{
+        transform: "translate(80vw,50vh) scale(2)"
+      }}>
+        <button onClick={add}>
+          add {num}
+        </button>
+
+        <button onClick={con}>
+          console {num}
+        </button>
       </main>
     </div>
   );
